0ccddf8d9fedc99e079d822739754d09e31f1a5b
Cobrettie <cobrettiegarner3@gmail.com>
2020-03-13 22:31:03 -0400

    begin functionality to  remove doubles from savedList

--- client/src/Movies/SavedList.js
+++ client/src/Movies/SavedList.js
@@ -5,13 +5,16 @@ const SavedList = props => (
   console.log(props),
   <div className="saved-list">
     <h3>Saved Movies:</h3>
-    {props.list.map(movie => (
+
+    {props.list.map(movie => ( 
+      
       <div>
         <NavLink to={`/movies/${movie.id}`}>
           <span className="saved-movie">{movie.title}</span>
         </NavLink>
       </div>
     ))}
+
     <Link to='/'>
       <div className="home-button">Home</div>
     </Link>
